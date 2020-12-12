bd8f429e39a086b8d1e14a6233c765c31778344a
unknown <gummi.bear456aaaaa@gmail.com>
2020-12-12 23:52:38 +0900

    ui

---
+++ client/src/Main.elm
@@ -0,0 +1,23 @@
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as D exposing (Decoder)
+
+main : Program () Model type Msg
+main =
+    Browser.element
+        {
+            init = init
+            , view = view
+            , update = update
+            , subscriptions = \_ -> Sub.none
+        }
+
+--MODEL
+
+type alias Model =
+    {
+        input : String
+    }
