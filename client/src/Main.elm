d835d0bb18d12300ba2ddc5871bda0b5bc427373
unknown <gummi.bear456aaaaa@gmail.com>
2021-02-26 00:05:44 +0900

    fix elm file

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,3 +1,5 @@
+module Main exposing (main)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -5,34 +7,61 @@ import Html.Events exposing (..)
 import Http
 import Json.Decode as D exposing (Decoder)
 
+
+
 --先にhaskellを書いて、取得するものを決める
+
 
 main : Program () Model Msg
 main =
     Browser.element
-        {
-            init = init
-            , view = view
-            , update = update
-            , subscriptions = \_ -> Sub.none
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
         }
+
+
 
 --MODEL
 
+
 type alias Model =
-    {
-        input : String
+    { input : String
     }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model ""
+    , Cmd.none
+    )
+
+
 
 --UPDATE
 
 
+type Msg
+    = Init
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Init ->
+            ( model, Cmd.none )
+
+
+
 --VIEW
+
+
 view : Model -> Html Msg
 view model =
-  div []
-    [
+    div []
+        []
 
-    ]
+
 
 --Data
