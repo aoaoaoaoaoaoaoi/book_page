31e2622b3030b15aac9a1179204493a88aaea28c
unknown <gummi.bear456aaaaa@gmail.com>
2021-03-16 23:50:16 +0900

    fixed some errors

--- backend/myproject/app/Main.hs
+++ backend/myproject/app/Main.hs
@@ -29,7 +29,15 @@ server top
     =    (liftIO $ return top)
     :<|> serveDirectoryWebApp "../../client"
 
-main :: IO ()
-main = do
+indexView :: IO()
+indexView = 
   top <- BS.readFile "../../client/index.html"
   run 8080 $ serve api (server top)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let arg1 = if (length args > 0) then Just (args !! 0) else indexView
+  case arg1 of
+    Just "migrate" -> doMigration
+    _ -> run 8080 app
